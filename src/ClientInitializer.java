8234d987be78a12bc23098bea1db133a8c7d37b0
TharuZika <maxtharuz4@gmail.com>
2022-08-11 11:06:22 +0530

    Add Error Messages

--- src/ClientInitializer.java
+++ src/ClientInitializer.java
@@ -15,11 +15,12 @@ public class ClientInitializer extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        Parent path = FXMLLoader.load(this.getClass().getResource("lk/ijse/gdse/view/ClientForm.fxml"));
+        Parent path = FXMLLoader.load(this.getClass().getResource("lk/ijse/gdse/view/LoginForm.fxml"));
         Scene mainScene = new Scene(path);
         primaryStage.setScene(mainScene);
         primaryStage.centerOnScreen();
-        primaryStage.initStyle(StageStyle.UNIFIED);
+        primaryStage.initStyle(StageStyle.DECORATED);
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 }
--- src/lk/ijse/gdse/controller/LoginFormController.java
+++ src/lk/ijse/gdse/controller/LoginFormController.java
@@ -1,4 +1,45 @@
 package lk.ijse.gdse.controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LoginFormController {
+    public JFXTextField txtIp;
+    public JFXTextField txtUserName;
+    public static String userName;
+    public static String ipAddress;
+    public AnchorPane rootPane;
+    public Label lblError;
+    public AnchorPane borderErrorUsername;
+    public AnchorPane borderErrorIp;
+
+    public void LoginOnAction(ActionEvent actionEvent) throws IOException {
+
+        if (txtUserName.getText().equals("")){
+
+            borderErrorIp.setVisible(true);
+            borderErrorUsername.setVisible(true);
+            lblError.setVisible(true);
+            txtIp.setStyle("-fx-faint-focus-color: red;" + "-fx-focus-color:red;" + "-jfx-unfocus-color:red");
+            txtUserName.setStyle("-fx-faint-focus-color: red;" + "-fx-focus-color:red;" + "-jfx-unfocus-color:red");
+            
+        }else {
+
+            userName = txtUserName.getText();
+            ipAddress = txtIp.getText();
+
+            Stage stage = (Stage) rootPane.getScene().getWindow();
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/lk/ijse/gdse/view/ClientForm.fxml"))));
+
+            System.out.println("IP Address: " + ipAddress + "\nUsername: " + userName);
+        }
+
+    }
 }
